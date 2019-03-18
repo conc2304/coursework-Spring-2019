4761f6da39d901322d23ae6dc6b98e06402702d5
clyzby <jose.conchello@gmail.com>
2019-03-18 10:28:58 -0400

    master  padd the time

--- public_html/clyzby/files/@custom/js/aboutTimeSketch_clock.js
+++ public_html/clyzby/files/@custom/js/aboutTimeSketch_clock.js
@@ -34,17 +34,21 @@ myp5Clock = function (sketch) {
 
   sketch.setup = function () {
     sketch.createCanvas(cnvClock.w, cnvClock.h);
-    sketch.background(0, 159, 0);
   };
 
   sketch.preload = function () {
   };
 
+  let angles;
   sketch.draw = function () {
-    sketch.background(100);
+
+    angles = getTimeInRadians();
 
     drawClockFace();
-    drawClockHands();
+    drawExtraShit(angles);
+    drawClockHands(angles);
+
+    showDigitalTime();
 
     // center point
     sClock.stroke(0);
@@ -88,70 +92,74 @@ function drawClockFace() {
   }
 }
 
+function showDigitalTime() {
+
+  let positionWithRespectToNoon = (Math.floor(sClock.hour() / 12) >= 1) ? 'PM' : 'AM';
+  let myTime = {
+    'h': (sClock.hour() % 12).toString(),
+    'm': (sClock.minute()).toString(),
+    's': (sClock.second()).toString(),
+  };
+
+  for (let val in myTime) {
+    if (myTime[val].length !== 2) {
+      myTime[val] = '0' + myTime[val];
+    }
+  }
+  let hh = myTime.h;
+  let mm = myTime.m;
+  let ss = myTime.s;
+
+
+  let displayTime = `${hh}:${mm}:${ss} ${positionWithRespectToNoon}`;
+
+  sClock.textSize(16);
+  sClock.fill(0, 0, 0);
+  sClock.text(displayTime, cnvClock.w * .66, (cnvClock.h / 2) + 2);
+}
 
 function drawClockHand(handPos, width) {
   sClock.strokeWeight(width);
   sClock.line(center.x, center.y, handPos.x, handPos.y);
 
-  sClock.strokeWeight(width * 2)
+  sClock.strokeWeight(width * 2);
   sClock.point(handPos.x, handPos.y);
 }
 
 
 function getTimeInRadians() {
   return {
-    second : sClock.map(sClock.second(), 0, 60, 0, sClock.TWO_PI) - sClock.HALF_PI,
-    minute : sClock.map(sClock.minute() + sClock.norm(sClock.second(), 0, 60), 0, 60, 0, sClock.TWO_PI) - sClock.HALF_PI,
-    hour : sClock.map(sClock.hour() + sClock.norm(sClock.minute(), 0, 60), 0, 24, 0, sClock.TWO_PI * 2) - sClock.HALF_PI,
+    second: sClock.map(sClock.second(), 0, 60, 0, sClock.TWO_PI) - sClock.HALF_PI,
+    minute: sClock.map(sClock.minute() + sClock.norm(sClock.second(), 0, 60), 0, 60, 0, sClock.TWO_PI) - sClock.HALF_PI,
+    hour: sClock.map(sClock.hour() + sClock.norm(sClock.minute(), 0, 60), 0, 24, 0, sClock.TWO_PI * 2) - sClock.HALF_PI,
   };
 }
 
-function getHandPosXY(angles) {
-  let handPos = {
-    second : {},
-    minute : {},
-    hour : {},
+
+function getHandPosXY(angle, length) {
+  return {
+    x: center.x + Math.cos(angle) * length,
+    y: center.y + Math.sin(angle) * length,
   };
-
-  for (let hand in clock.hands) {
-    handPos[hand].x = center.x + Math.cos(angles[hand]) * clock.hands[hand].length;
-    handPos[hand].y = center.y + Math.sin(angles[hand]) * clock.hands[hand].length;
-  }
-
-  return handPos;
 }
 
-function drawClockHands() {
+
+function drawClockHands(angles) {
 
   // shift starting point to be 12 and not 3 by subtracting half pi
-  let angles = getTimeInRadians();
-  let handPos = getHandPosXY(angles);
-
-
-  sClock.noFill();
-  sClock.stroke(100);
-  sClock.strokeWeight(1);
-
-  sClock.beginShape();
-  sClock.vertex(center.x, center.y);
-  sClock.vertex(center.x, center.y);
-  for (let hand in handPos) {
-    sClock.curveVertex(handPos[hand].x, handPos[hand].y);
-    console.log(hand);
-  }
-  sClock.vertex(center.x, center.y);
-  sClock.vertex(center.x, center.y);
-  sClock.endShape();
-
-
+  let handPos = {
+    second: getHandPosXY(angles.second, clock.hands.second.length),
+    minute: getHandPosXY(angles.minute, clock.hands.minute.length),
+    hour: getHandPosXY(angles.hour, clock.hands.hour.length),
+  };
 
   sClock.stroke(10);
   for (let hand in clock.hands) {
-    drawClockHand(handPos[hand], clock.hands[hand].width );
+    drawClockHand(handPos[hand], clock.hands[hand].width);
   }
   myClear();
-
 }
+
 
 function myClear() {
   sClock.noStroke();
@@ -159,23 +167,33 @@ function myClear() {
 }
 
 
-function drawExtraShit() {
-
+function drawExtraShit(angles) {
+  let iter = 9;
+  let handPos = {};
 
   sClock.noFill();
-  sClock.stroke(100);
   sClock.strokeWeight(1);
 
-  sClock.beginShape();
-  sClock.vertex(center.x, center.y);
-  sClock.vertex(center.x, center.y);
-  for (let hand in handPos) {
-    sClock.curveVertex(handPos[hand].x, handPos[hand].y);
-    console.log(hand);
+  let frequency = 1;
+  for (let i = 0; i <= iter; i++) {
+
+    red = Math.sin(frequency * i + 2 + (sClock.frameCount * .03 % 255)) * 127 + 128;
+    green = Math.sin(frequency * i + 4 + (sClock.frameCount * .03 % 255)) * 127 + 128;
+    blue = Math.sin(frequency * i + 8 + (sClock.frameCount * .03 % 255)) * 127 + 128;
+
+    sClock.stroke(red, green, blue, 255 / i);
+
+    sClock.beginShape();
+    sClock.vertex(center.x, center.y);
+    sClock.vertex(center.x, center.y);
+
+    for (let hand in clock.hands) {
+      handPos[hand] = getHandPosXY(angles[hand], clock.hands[hand].length / i);
+      sClock.curveVertex(handPos[hand].x, handPos[hand].y);
+    }
+
+    sClock.vertex(center.x, center.y);
+    sClock.vertex(center.x, center.y);
+    sClock.endShape();
   }
-  sClock.vertex(center.x, center.y);
-  sClock.vertex(center.x, center.y);
-  sClock.endShape();
-
-
 }
