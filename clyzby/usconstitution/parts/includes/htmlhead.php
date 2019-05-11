0f96ed4df1aa25dc02dc3c603b1378b7116a6108
clyzby <jose.conchello@gmail.com>
2019-05-11 10:32:47 -0400

    master - fix file paths  add portrait and work on index.html

--- clyzby/usconstitution/parts/includes/htmlhead.php
+++ clyzby/usconstitution/parts/includes/htmlhead.php
@@ -3,4 +3,4 @@
 <meta name=viewport content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />
-<link rel="stylesheet" href="/~jconchel/clyzby/usconstitution/parts/css/constitution.css" />
+<link rel="stylesheet" href="clyzby/usconstitution/parts/css/constitution.css" />
