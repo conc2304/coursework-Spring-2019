b7bec5cdd7d03ae4ceb8f3626386493aed663567
clyzby <jose.conchello@gmail.com>
2019-06-19 10:03:34 -0400

    master - reset the audio file

--- clyzby/p5js/js/src/waveDrawer_w_audio.js
+++ clyzby/p5js/js/src/waveDrawer_w_audio.js
@@ -36,8 +36,8 @@ let s = (sketch) => {
     sketch.objects.glock = myp5.loadModel('files/3d_obj/Glock 3d.obj', true);
     // todo find a way to display the name of the audio file  // hard code the original maybe? parse it out from the end of the file name?
     // audio = myp5.loadSound('/clyzby/p5js/files/audio/CharlestheFirst - Chynna - The Conversation.wav');
-    // audio = myp5.loadSound('/clyzby/p5js/files/audio/PUFF - TSURUDA x HUXLEY ANNE.mp3');
-    audio = myp5.loadSound('/clyzby/p5js/files/audio/Pushloop - Deep, Dark & Dangerous Mix 015.mp3');
+    audio = myp5.loadSound('/clyzby/p5js/files/audio/PUFF - TSURUDA x HUXLEY ANNE.mp3');
+    // audio = myp5.loadSound('/clyzby/p5js/files/audio/Pushloop - Deep, Dark & Dangerous Mix 015.mp3');
     audio.pause();
     fft = new p5.FFT();
 
