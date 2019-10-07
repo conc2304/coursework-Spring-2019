d439d1aaa31b7b04b67f491f1e71afc350e0083a
clyzby <jose.conchello@gmail.com>
2019-10-07 15:14:11 -0400

    posenet  - get the goids to follow

--- clyzby/p5js/js/src/poseNetService.js
+++ clyzby/p5js/js/src/poseNetService.js
@@ -157,12 +157,13 @@ PoseDetector.prototype.render = function() {
   // We can call both functions to draw all keypoints and the skeletons
 
   if (this.mode.currentValue === "Flocking") {
-    flock.run();
     //   } else {
     // this.drawTrailers();
     if (flipHorizintal) {
       myp5.scale(-1.0, 1.0);
     }
+    flock.run();
+
     this.drawKeypoints();
     this.drawSkeleton();
     if (flipHorizintal) {
@@ -174,6 +175,10 @@ PoseDetector.prototype.render = function() {
 // A function to draw ellipses over the detected keypoints
 PoseDetector.prototype.drawKeypoints = function() {
   // Loop through all the poses detected
+  if (!poses) {
+      return;
+  }
+  
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
@@ -182,11 +187,15 @@ PoseDetector.prototype.drawKeypoints = function() {
     myp5.strokeWeight(3);
     myp5.stroke(this.hue.currentValue, this.saturation.currentValue, 100);
     this.renderPose(pose);
+
   }
 };
 
 PoseDetector.prototype.drawSkeleton = function() {
   // Loop through all the skeletons detected
+  if (!poses) {
+      return;
+  }
   for (let i = 0; i < poses.length; i++) {
     let skeleton = poses[i].skeleton;
     // For every skeleton, loop through all body connections
@@ -230,8 +239,7 @@ PoseDetector.prototype.renderPose = function(pose) {
     // A keypoint is an object describing a body part (like rightArm or leftShoulder)
     let keypoint = pose.keypoints[j];
     if (keypoint.score > 0.2) {
-      // console.log('render');
-      // console.log({x: keypoint.position.x - this.windowWidth / 2, y: keypoint.position.y - this.windowHeight / 2, part: keypoint.part});
+
       this.renderShape(
         keypoint.position.x - this.windowWidth / 2,
         keypoint.position.y - this.windowHeight / 2,
@@ -316,193 +324,195 @@ class Flock {
 // The Nature of Code
 // Daniel Shiffman
 // http://natureofcode.com
-
 // Boid class
 // Methods for Separation, Cohesion, Alignment added
-
-function Boid(x, y) {
-  this.acceleration = myp5.createVector(0, 0);
-  this.velocity = myp5.createVector(myp5.random(-1, 1), myp5.random(-1, 1));
-  if (flipHorizintal) {
-    this.position = myp5.createVector(-x, y);
-  } else {
-    this.position = myp5.createVector(x, y);
-  }
-  this.r = 3.0;
-  this.maxspeed = 15; // Maximum speed
-  this.maxforce = 1; // Maximum steering force
+class Boid {
+    constructor(x, y) {
+        this.acceleration = myp5.createVector(0, 0);
+        this.velocity = myp5.createVector(myp5.random(-1, 1), myp5.random(-1, 1));
+        if (flipHorizintal) {
+            this.position = myp5.createVector(-x, y);
+        }
+        else {
+            this.position = myp5.createVector(x, y);
+        }
+        this.r = 5;
+        this.maxspeed = 12; // Maximum speed
+        this.maxforce = .8; // Maximum steering force
+    }
+    run(boids) {
+        this.flock(boids);
+        this.updateBoid();
+        this.boidBorders();
+        this.renderBoid();
+    }
+    applyForce(force) {
+        // We could add mass here if we want A = F / M
+        this.acceleration.add(force);
+    }
+    // We accumulate a new acceleration each time based on three rules
+    flock(boids) {
+        let sep = this.separateBoids(boids); // Separation
+        let ali = this.alignBoids(boids); // Alignment
+        let coh = this.boidCohesion(boids); // Cohesion
+        // Arbitrarily weight these forces
+        sep.mult(1.5);
+        ali.mult(1.0);
+        coh.mult(1.0);
+        // Add the force vectors to acceleration
+        this.applyForce(sep);
+        this.applyForce(ali);
+        this.applyForce(coh);
+    }
+    // Method to update location
+    updateBoid() {
+        // Update velocity
+        this.velocity.add(this.acceleration);
+        // Limit speed
+        this.velocity.limit(this.maxspeed);
+        this.position.add(this.velocity);
+        // Reset accelertion to 0 each cycle
+        this.acceleration.mult(0);
+    }
+    // A method that calculates and applies a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+    seekBoid(target) {
+        let nearestTarget = getNearestTartget(this.position);
+        // console.log(nearestTarget);
+        // console.log(nearestTarget);
+        let homingDirection = myp5.createVector(nearestTarget.x, nearestTarget.y);
+        //   let homingDirection = myp5.createVector(myp5.mouseX, myp5.mouseY);
+        let desired = p5.Vector.sub(homingDirection, this.position); // A vector pointing from the location to the target
+        // Normalize desired and scale to maximum speed
+        desired.normalize();
+        desired.mult(this.maxspeed);
+        // Steering = Desired minus Velocity
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce); // Limit to maximum steering force
+        return steer;
+    }
+    renderBoid() {
+        // Draw a triangle rotated in the direction of velocity
+        let theta = this.velocity.heading() + radians(90);
+        myp5.noFill();
+        myp5.stroke(200, 200, 200);
+        myp5.strokeWeight(2);
+        myp5.push();
+        myp5.translate(this.position.x - myp5.width / 2, this.position.y - myp5.height / 2);
+        myp5.rotate(theta);
+        myp5.beginShape();
+        myp5.vertex(0, -this.r * 2);
+        myp5.vertex(-this.r, this.r * 5);
+        myp5.vertex(this.r, this.r * 5);
+        myp5.endShape(CLOSE);
+        myp5.pop();
+    }
+    // Wraparound
+    boidBorders() {
+        if (this.position.x < -this.r)
+            this.position.x = myp5.width + this.r;
+        if (this.position.y < -this.r)
+            this.position.y = myp5.height + this.r;
+        if (this.position.x > myp5.width + this.r)
+            this.position.x = -this.r;
+        if (this.position.y > myp5.height + this.r)
+            this.position.y = -this.r;
+    }
+    // Separation
+    // Method checks for nearby boids and steers away
+    separateBoids(boids) {
+        let desiredseparation = 25.0;
+        let steer = myp5.createVector(0, 0);
+        let count = 0;
+        // For every boid in the system, check if it's too close
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position, boids[i].position);
+            // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+            if (d > 0 && d < desiredseparation) {
+                // Calculate vector pointing away from neighbor
+                let diff = p5.Vector.sub(this.position, boids[i].position);
+                diff.normalize();
+                diff.div(d); // Weight by distance
+                steer.add(diff);
+                count++; // Keep track of how many
+            }
+        }
+        // Average -- divide by how many
+        if (count > 0) {
+            steer.div(count);
+        }
+        // As long as the vector is greater than 0
+        if (steer.mag() > 0) {
+            // Implement Reynolds: Steering = Desired - Velocity
+            steer.normalize();
+            steer.mult(this.maxspeed);
+            steer.sub(this.velocity);
+            steer.limit(this.maxforce);
+        }
+        return steer;
+    }
+    // Alignment
+    // For every nearby boid in the system, calculate the average velocity
+    alignBoids(boids) {
+        let neighbordist = 50;
+        let sum = createVector(0, 0);
+        let count = 0;
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position, boids[i].position);
+            if (d > 0 && d < neighbordist) {
+                sum.add(boids[i].velocity);
+                count++;
+            }
+        }
+        if (count > 0) {
+            sum.div(count);
+            sum.normalize();
+            sum.mult(this.maxspeed);
+            let steer = p5.Vector.sub(sum, this.velocity);
+            steer.limit(this.maxforce);
+            return steer;
+        }
+        else {
+            return createVector(0, 0);
+        }
+    }
+    // Cohesion
+    // For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
+    boidCohesion(boids) {
+        let neighbordist = 50;
+        let sum = myp5.createVector(0, 0); // Start with empty vector to accumulate all locations
+        let count = 0;
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.position, boids[i].position);
+            if (d > 0 && d < neighbordist) {
+                sum.add(boids[i].position); // Add location
+                count++;
+            }
+        }
+        if (count > 0) {
+            sum.div(count);
+            return this.seekBoid(sum); // Steer towards the location
+        }
+        else {
+            return myp5.createVector(0, 0);
+        }
+    }
 }
 
-Boid.prototype.run = function(boids) {
-  this.flock(boids);
-  this.update();
-  this.borders();
-  this.render();
-};
 
-Boid.prototype.applyForce = function(force) {
-  // We could add mass here if we want A = F / M
-  this.acceleration.add(force);
-};
 
-// We accumulate a new acceleration each time based on three rules
-Boid.prototype.flock = function(boids) {
-  let sep = this.separate(boids); // Separation
-  let ali = this.align(boids); // Alignment
-  let coh = this.cohesion(boids); // Cohesion
-  // Arbitrarily weight these forces
-  sep.mult(1.5);
-  ali.mult(1.0);
-  coh.mult(1.0);
-  // Add the force vectors to acceleration
-  this.applyForce(sep);
-  this.applyForce(ali);
-  this.applyForce(coh);
-};
 
-// Method to update location
-Boid.prototype.update = function() {
-  // Update velocity
-  this.velocity.add(this.acceleration);
-  // Limit speed
-  this.velocity.limit(this.maxspeed);
-  this.position.add(this.velocity);
-  // Reset accelertion to 0 each cycle
-  this.acceleration.mult(0);
-};
 
-// A method that calculates and applies a steering force towards a target
-// STEER = DESIRED MINUS VELOCITY
-Boid.prototype.seek = function(target) {
-  let mouseX = flipHorizintal ? -myp5.mouseX : myp5.mouseX;
-  //   let nearestTarget = getNearestTartget(this.position);
-  //   console.log(nearestTarget);
-  //   let homingDirection = myp5.createVector(nearestTarget.x, nearestTarget.y);
-  console.log(-myp5.mouseX);
-  console.log(myp5.mouseX);
-  let homingDirection = myp5.createVector(myp5.mouseX, myp5.mouseY);
-  let desired = p5.Vector.sub(homingDirection, this.position); // A vector pointing from the location to the target
-  // Normalize desired and scale to maximum speed
-  desired.normalize();
-  desired.mult(this.maxspeed);
-  // Steering = Desired minus Velocity
-  let steer = p5.Vector.sub(desired, this.velocity);
-  steer.limit(this.maxforce); // Limit to maximum steering force
-  return steer;
-};
 
-Boid.prototype.render = function() {
-  // Draw a triangle rotated in the direction of velocity
-  let theta = this.velocity.heading() + radians(90);
-  myp5.fill(127);
-  myp5.stroke(200);
-  myp5.strokeWeight(1);
-  myp5.push();
-  myp5.translate(
-    this.position.x - myp5.width / 2,
-    this.position.y - myp5.height / 2
-  );
-  myp5.rotate(theta);
-  myp5.beginShape();
-  myp5.vertex(0, -this.r * 2);
-  myp5.vertex(-this.r, this.r * 5);
-  myp5.vertex(this.r, this.r * 5);
-  myp5.endShape(CLOSE);
-  myp5.pop();
-};
 
-// Wraparound
-Boid.prototype.borders = function() {
-  if (this.position.x < -this.r) this.position.x = myp5.width + this.r;
-  if (this.position.y < -this.r) this.position.y = myp5.height + this.r;
-  if (this.position.x > myp5.width + this.r) this.position.x = -this.r;
-  if (this.position.y > myp5.height + this.r) this.position.y = -this.r;
-};
 
-// Separation
-// Method checks for nearby boids and steers away
-Boid.prototype.separate = function(boids) {
-  let desiredseparation = 25.0;
-  let steer = myp5.createVector(0, 0);
-  let count = 0;
-  // For every boid in the system, check if it's too close
-  for (let i = 0; i < boids.length; i++) {
-    let d = p5.Vector.dist(this.position, boids[i].position);
-    // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-    if (d > 0 && d < desiredseparation) {
-      // Calculate vector pointing away from neighbor
-      let diff = p5.Vector.sub(this.position, boids[i].position);
-      diff.normalize();
-      diff.div(d); // Weight by distance
-      steer.add(diff);
-      count++; // Keep track of how many
-    }
-  }
-  // Average -- divide by how many
-  if (count > 0) {
-    steer.div(count);
-  }
 
-  // As long as the vector is greater than 0
-  if (steer.mag() > 0) {
-    // Implement Reynolds: Steering = Desired - Velocity
-    steer.normalize();
-    steer.mult(this.maxspeed);
-    steer.sub(this.velocity);
-    steer.limit(this.maxforce);
-  }
-  return steer;
-};
 
-// Alignment
-// For every nearby boid in the system, calculate the average velocity
-Boid.prototype.align = function(boids) {
-  let neighbordist = 50;
-  let sum = createVector(0, 0);
-  let count = 0;
-  for (let i = 0; i < boids.length; i++) {
-    let d = p5.Vector.dist(this.position, boids[i].position);
-    if (d > 0 && d < neighbordist) {
-      sum.add(boids[i].velocity);
-      count++;
-    }
-  }
-  if (count > 0) {
-    sum.div(count);
-    sum.normalize();
-    sum.mult(this.maxspeed);
-    let steer = p5.Vector.sub(sum, this.velocity);
-    steer.limit(this.maxforce);
-    return steer;
-  } else {
-    return createVector(0, 0);
-  }
-};
-
-// Cohesion
-// For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
-Boid.prototype.cohesion = function(boids) {
-  let neighbordist = 50;
-  let sum = myp5.createVector(0, 0); // Start with empty vector to accumulate all locations
-  let count = 0;
-  for (let i = 0; i < boids.length; i++) {
-    let d = p5.Vector.dist(this.position, boids[i].position);
-    if (d > 0 && d < neighbordist) {
-      sum.add(boids[i].position); // Add location
-      count++;
-    }
-  }
-  if (count > 0) {
-    sum.div(count);
-    return this.seek(sum); // Steer towards the location
-  } else {
-    return myp5.createVector(0, 0);
-  }
-};
 
 function getNearestTartget(seeker) {
   let distance = null;
   let position = { x: 0, y: 0 };
+
 
   if (!poses || poses.length <= 0 || !poses[0].pose.keypoints) {
     return position;
@@ -512,12 +522,14 @@ function getNearestTartget(seeker) {
   for (let i = 0; i < keypoints.length; i++) {
     // A keypoint is an object describing a body part (like rightArm or leftShoulder)
     let keypoint = keypoints[i];
-    if (keypoint.score < 0.2) {
+    if (keypoint.score < 0.2 || keypoint.position.x > myp5.width || keypoint.position.x < 0 || keypoint.position.y < 0 || keypoint.position.y > myp5.height ) {
       continue;
     }
 
-    keypoint.position.x -= this.windowWidth / 2;
-    keypoint.position.y -= this.windowHeight / 2;
+    if (!['nose', 'rightWrist', 'leftWrist', 'rightAnkle', 'leftAnkle'].includes(keypoint.part)) {
+        continue;
+    }
+
     nextDistance = getDistance(seeker, keypoint.position);
 
     if (distance === null) {
@@ -538,6 +550,8 @@ function getNearestTartget(seeker) {
       };
     }
   }
+
+
   return position;
 }
 
